840779ea72106f20890f14051d53414f11c7f80a
Thando Mafela <thandomafela@hotmail.com>
2013-04-07 22:38:36 +0100

    sss

--- tlc/grails-app/conf/BuildConfig.groovy
+++ tlc/grails-app/conf/BuildConfig.groovy
@@ -1,13 +1,13 @@
-grails.servlet.version = "3.0" // Change depending on target container compliance (2.5 or 3.0)
+grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 // uncomment (and adjust settings) to fork the JVM to isolate classpaths
-//grails.project.fork = [
-//   run: [maxMemory:1024, minMemory:64, debug:false, maxPerm:256]
-//]
+grails.project.fork = [
+   run: [maxMemory:1024, minMemory:64, debug:false, maxPerm:256]
+]
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
